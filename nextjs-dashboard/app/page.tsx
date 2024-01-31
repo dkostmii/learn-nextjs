a9f6401dd01ced9fc4c08475f98f3dde8435b78a
Dmytro-Andrii Kostelnyi <dmitrokostelny95@gmail.com>
2024-01-31 18:02:04 +0200

    Add images
    Add responsive images, according to Chapter 3.

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -1,6 +1,7 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import Image from 'next/image';
 
 import styles from '@/app/ui/home.module.css';
 import { lusitana } from '@/app/ui/fonts';
@@ -37,6 +38,20 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of dashboard project showing desktop version"
+          />
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="block md:hidden"
+            alt="Screenshots of dashboard project showing desktop version"
+          />
         </div>
       </div>
     </main>
