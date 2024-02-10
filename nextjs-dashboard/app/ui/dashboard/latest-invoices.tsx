70874b87f0f17f56806025c88bf0097b91adf0a0
Dmytro-Andrii Kostelnyi <dmitrokostelny95@gmail.com>
2024-02-10 13:24:47 +0200

    Apply streaming
    Stream whole Dashboard page. Stream individual components. Group `loading.tsx` and `page.tsx`. Create a wrapper for cards.

--- nextjs-dashboard/app/ui/dashboard/latest-invoices.tsx
+++ nextjs-dashboard/app/ui/dashboard/latest-invoices.tsx
@@ -1,13 +1,12 @@
-import { ArrowPathIcon } from "@heroicons/react/24/outline";
-import clsx from "clsx";
-import Image from "next/image";
-import { lusitana } from "@/app/ui/fonts";
-import { LatestInvoice } from "@/app/lib/definitions";
-export default async function LatestInvoices({
-  latestInvoices,
-}: {
-  latestInvoices: LatestInvoice[];
-}) {
+import { ArrowPathIcon } from '@heroicons/react/24/outline';
+import clsx from 'clsx';
+import Image from 'next/image';
+import { lusitana } from '@/app/ui/fonts';
+import { fetchLatestInvoices } from '@/app/lib/data';
+
+export default async function LatestInvoices() {
+  const latestInvoices = await fetchLatestInvoices();
+
   return (
     <div className="flex w-full flex-col md:col-span-4">
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -20,9 +19,9 @@ export default async function LatestInvoices({
               <div
                 key={invoice.id}
                 className={clsx(
-                  "flex flex-row items-center justify-between py-4",
+                  'flex flex-row items-center justify-between py-4',
                   {
-                    "border-t": i !== 0,
+                    'border-t': i !== 0,
                   },
                 )}
               >
